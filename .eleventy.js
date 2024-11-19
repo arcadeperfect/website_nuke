424cd23fd22a9b90454ff22b9c37450fb5a69e7c
arcadeperfect <alexander.harding@gmail.com>
2024-11-18 23:12:30 -0500

    remove prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,3 @@
-// const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
-// module.exports = function(eleventyConfig) {
-//   // Add syntax highlighting
-//   eleventyConfig.addPlugin(syntaxHighlight);
-  
-//   // Copy static assets
-//   eleventyConfig.addPassthroughCopy("src/css");
-//   eleventyConfig.addPassthroughCopy("src/js");
-//   eleventyConfig.addPassthroughCopy("src/assets");
-
-//   // Configure Nunjucks to ignore unknown variables
-//   eleventyConfig.setNunjucksEnvironmentOptions({
-//     throwOnUndefined: false,
-//     autoescape: false
-//   });
-
-//   return {
-//     pathPrefix: "/website_nuke/",
-//     dir: {
-//       input: "src",
-//       output: "_site",
-//       includes: "_includes",
-//       layouts: "_layouts"
-//     },
-//     templateFormats: ["md", "njk"],
-//     markdownTemplateEngine: "njk"
-//   };
-// };
-
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
@@ -39,7 +9,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/js");
 
   return {
-    pathPrefix: process.env.ELEVENTY_ENV === "production" ? "/website_nuke/" : "/",
+    pathPrefix: "/",
     dir: {
       input: "src",
       output: "_site",
